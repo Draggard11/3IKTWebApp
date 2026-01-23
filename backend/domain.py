237ace60a64422b0f266015c3b04a40ba3a44139
muldvarpsen <samuel.knudsen@nhokp.com>
2026-01-23 08:51:37 +0100

    enhance User class to include password attribute and setter method

--- backend/domain.py
+++ backend/domain.py
@@ -2,10 +2,12 @@ class User:
 
     id
     username = ""
+    password = ""
 
-    def __init__(self, id, username):
+    def __init__(self, id, username, password):
         self.id = id
         self.username = username
+        self.password = password
 
     def getUsername(self):
         return  self.username
@@ -13,12 +15,30 @@ class User:
     def setUsername(self, username):
         self.username = username
 
+    def setPassword(self, password):
+        self.password = password
+
 class Blog:
     
     def __init__(self):
         pass
 
+    def Post(self, title, madeBy, publishedAt, comments):
+        self.title = title
+        self.
+
 class Comment:
 
     def __init__(self):
         pass
+
+
+
+
+
+
+
+
+
+
+#Bạn có biết những con gà này không có màu xanh lá cây và hôm này con không ăn bánh mì cho bữa sáng phải không?
