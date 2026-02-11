8d21672fadb50ba3e89a6278c4bb0eee920513f9
Dragg <draggard@duck.com>
2026-02-11 09:53:43 +0100

    added test_comments tests

--- backend/tests/test_comments.py
+++ backend/tests/test_comments.py
@@ -4,8 +4,28 @@ def test_init():
     user = User(0, "username", "passwd")
     blog = Blog(user)
     comment = Comment(user, blog)
+    assert comment.commenter == user
+    assert comment.blog == blog
+    
 
-    comment.post("Arvid Gimre.", 5)
+def test_post():
+    user = User(0, "username", "passwd")
+    blog = Blog(user)
+    comment = Comment(user, blog)
 
-    assert comment.text == "Arvid Gimre."
-    assert comment.stars == 5
+    #check for stars upper and lower bound
+    for i in [1,2,3,4,5]:
+        assert comment.post("...", i)
+    assert not comment.post("...", -1)
+    assert not comment.post("...", 0)
+    assert not comment.post("...", 6)
+
+    # check for comment text upper and lower bound
+    assert not comment.post("", 1)
+    assert comment.post("A" * 50, 1)
+    assert not comment.post("A" * 51, 1)
+
+    
+
+def test_edit():
+    pass
