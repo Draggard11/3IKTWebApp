ede0b3b2ab07755604715b65969016ae6f9b535e
muldvarpsen <samuel.knudsen@nhokp.com>
2026-03-09 12:13:42 +0100

    feat: implement showAllText function to handle blog text display

--- frontend/frontend/src/pages/Home.tsx
+++ frontend/frontend/src/pages/Home.tsx
@@ -4,16 +4,17 @@ import GetBlogs from "../components/GetBlogs";
 
 const Blogs = () => {
   const blogs = GetBlogs();
-  const maxChars = 30;
 
-  const 
+  const showAllText = (index) => {
+    const []
+  }
 
   return (
     <>
       {blogs.map((blog, index) => (
         <div key={index} id='blog-title-card'>
           <h2>{blog.title}</h2>
-          <p style={{fontSize: 16}}>{blog.text}<button onClick={() => showMoreText(index)}>Show more for £19.99</button> </p>
+          <p style={{fontSize: 16}}>{showAllText ? blog.text.slice(0, 50) + '... '}<button onClick={() => showAllText(index)}>Show more for £19.99</button> </p>
           <div>
             <p style={{fontWeight:"bold", fontSize: 16}}>Commments: <span style={{fontWeight:"lighter"}}>{blog.comments.length}</span></p>
             {/* {blog.comments.map((comment:any, index:number) => (
