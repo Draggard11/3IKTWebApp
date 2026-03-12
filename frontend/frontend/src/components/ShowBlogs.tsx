61c20e3a9e479d87c6a834ae34b2fbdb5e1c157b
muldvarpsen <samuel.knudsen@nhokp.com>
2026-03-12 14:07:41 +0100

    feat: improve blog card functionality and styling with enhanced comment display

--- frontend/frontend/src/components/ShowBlogs.tsx
+++ frontend/frontend/src/components/ShowBlogs.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import '../styles/ShowBlogs.css';
 
@@ -20,37 +19,42 @@ export function BlogCard({ blog }: { blog: Blog }) {
 
     const [canShowMoreText, setCanShowMoreText] = useState<boolean>(false);
 
-    const ShowText = () => {
-        setCanShowMoreText(true);
-    }
+    const [showComments, setShowComments] = useState<boolean>(false);
 
-    const ShowComments = () => {
+    const Comments = () => {
         return (
-            <p>Xin chao</p>
+            <div id='comment-section'>
+                {blog.comments.map((comment) => (
+                    <div id='comments-row'>
+                        <h4>{comment.commenter} - 
+                            <span style={{borderColor: 'yellow'}}>
+                                <span style={{color: 'transparent', userSelect: 'none',}}>_</span>
+                                    {comment.stars}
+                                <span style={{color: 'transparent', userSelect: 'none'}}>_</span>
+                            </span>
+                        </h4>
+                        <p style={{fontWeight: 100}}>{comment.text}</p>
+                    </div>
+                ))}
+            </div>
         )
     }
     
     return (
         <div id="blog-title-card">
           <h2>{blog.title}</h2>
-            <p style={{fontSize: 20}}>{!canShowMoreText ? blog.text.slice(0, 50) + '...' : blog.text}
+            <p id='blog-text'>{!canShowMoreText ? blog.text.slice(0, 50) + '...' : blog.text}
                 <span style={{color: 'transparent', userSelect: 'none'}}>__</span>
-                <button key={blog.id} onClick={() => ShowText()}>Show more</button>
+                <button key={blog.id} onClick={() => setCanShowMoreText(!canShowMoreText)}>{canShowMoreText ? 'Hide text' : 'Show text'}</button>
             </p>
-            <div>
-                <p style={{fontWeight:"bold", fontSize: 16}}>Commments:
+            <div id='comment-section-container'>
+                <p style={{fontWeight:"bold", fontSize: 16}}>
+                    Comments:
                     <span style={{fontWeight:"lighter"}}>{blog.comments.length}</span>
                     <span style={{color: 'transparent', userSelect: 'none'}}>_</span>
-                    <button key={blog.id} onClick={() => ShowComments()}>Show comments</button>
-                    <div id='comment-section'>
-                        {blog.comments.map((comment) => (
-                            <div id='comments'>
-                                <h4 style={{marginLeft: 8, width: 'fit-content', paddingRight: 12}}>{comment.commenter}</h4>
-                                <p style={{fontWeight: 100}}>{comment.text}</p>
-                            </div>
-                        ))}
-                    </div>
+                    <button onClick={() => setShowComments(!showComments)}>{showComments ? 'Hide comments' : 'Show comments'}</button>
                 </p>
+                {showComments ? Comments() : ''}
             </div>
         </div>
    )
