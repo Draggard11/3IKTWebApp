6295bcd03a301850309e7fa9f20dd9aaea373890
Landy <leanderrv07@gmail.com>
2026-03-13 09:14:40 +0100

    123

--- backend/tests/test_blog.py
+++ backend/tests/test_blog.py
@@ -5,8 +5,16 @@ def test_init():
     blog = Blog(user)
     assert blog.madeBy == user
 
-def test_user_make_blog_post():
+def test_post():
     user = User(0, "Bob", "pass123")
-    blog = user.makeBlogPost("My First Blog", "This is the content of my first blog post.")
-    assert isinstance(blog, Blog)
-    assert blog in user.blogs
+    blog = Blog(user)
+    comment = Comment(user, blog)
+    comment.post("Nice post!", 1)
+    assert blog.post("Hello World", "more text", [comment, comment])
+
+    assert blog.post("", "more text", [comment, comment]) == False # no text and title given
+    assert blog.post(f"{'A ' * 50}", f"{'A ' * 1000}", [comment, comment]) == False # too long title
+    assert blog.post(f"{'A ' * 1000}", f"{'A ' * 50}", [comment, comment]) == False # too long text
+
+def test_edit():
+    pass
