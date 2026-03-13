8865674460e42d38397424669dcb3b35fa675733
muldvarpsen <samuel.knudsen@nhokp.com>
2026-03-13 09:56:02 +0100

    feat: implement pagination and display count for blog posts

--- frontend/frontend/src/components/GetBlogs.tsx
+++ frontend/frontend/src/components/GetBlogs.tsx
@@ -7,20 +7,32 @@ import mockBlogs from './MockBlogs.json';
 
 function GetBlogs() {
     const [allBlogs, setAllBlogs] = useState<Blog[]>([]);
+    const [blogCounter, setBlogCounter] = useState<number>(0);
+    const [pageNumber, setPageNumber] = useState<number>(1);
+
+    const displayNBlogs = allBlogs.slice(0, blogCounter);
 
     useEffect(() => {
         setAllBlogs(mockBlogs);
+        setBlogCounter(20);
     }, []);
 
     return (
         <div id="blogs-container">
             <div id="blogs">
-                {allBlogs.map((blog) => (
+                <p>Showing {displayNBlogs.length} blogs</p>
+                {}
+                {displayNBlogs.map((blog) => (
                     <>
                         <BlogCard key={blog.id} blog={blog} />
                         {/*<button onClick={() => ExpandBlog(blog.id)}>Show more for £19.95</button>*/}
                     </>
                 ))}
+            </div>
+            <p style={{marginBottom: 8}}>Page {pageNumber}</p>
+            <div id="switch-page-container">
+                <button className="switch-page">{'<'} Last page</button>
+                <button className="switch-page">Next page {'>'}</button>
             </div>
         </div>
     )
