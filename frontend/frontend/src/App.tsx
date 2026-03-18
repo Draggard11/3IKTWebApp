5a3431b7c7068d595eab3388e2d573426ac7d7f1
Draggard11 <draggard@duck.com>
2026-03-18 10:52:04 +0100

    added getusername to frontend

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -3,17 +3,19 @@ import "./styles/App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MakeBlogs from "./components/MakeBlogs"
+import GetUsername from "./components/GetUsername";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <div id="container">
-          <h1 style={{fontSize: 38}}>Vietnamese Blog</h1>
           <nav id="navlink">
             <NavLink to="/"> Home</NavLink> |
             <NavLink to="/login"> Login/Register</NavLink>
+            <a><GetUsername /></a>
           </nav>
+          <h1 style={{fontSize: 38}}>Vietnamese Blog</h1>
         </div>
         <MakeBlogs />
         <br /><br />
