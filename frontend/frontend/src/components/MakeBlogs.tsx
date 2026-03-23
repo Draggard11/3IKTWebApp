097ef1d7b35190105fe981fc5612a9221785f141
Landy <leanderrv07@gmail.com>
2026-03-23 10:08:21 +0100

    blog post ting

--- frontend/frontend/src/components/MakeBlogs.tsx
+++ frontend/frontend/src/components/MakeBlogs.tsx
@@ -3,15 +3,22 @@ const MakeBlogs = () => {
     
     return (
         <>
-        
-        </>
-    )
-}
-
-function makeBlogs() {
-    return (
-        <>
-        
+        <form method="post">
+            <label>
+                Post title: <input name="myInput" />
+            </label>
+            <label>
+                Edit your post:
+                <textarea 
+                    name="postContent"
+                    defaultValue="What's on your mind?"
+                    rows={4}
+                    cols={40}
+                />
+            </label>
+            <hr />
+            <button type="submit">Post</button>
+        </form>
         </>
     )
 }
