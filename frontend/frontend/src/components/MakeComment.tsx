1e846b0f4ffc595ed1b33c9b57416b14558c6b38
Draggard11 <draggard@duck.com>
2026-03-27 09:52:58 +0100

    start of frontend api connection

--- frontend/frontend/src/components/MakeComment.tsx
+++ frontend/frontend/src/components/MakeComment.tsx
@@ -10,13 +10,50 @@ export default function MakeComment() {
     const [text, setText] = useState<string>();
     const [stars, setStars] = useState<number>();
 
+    async function postComment() {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/api/comment', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    commenter: commenter,
+                    text: text,
+                    stars: stars,
+                })
+            })
+
+            if (!response.ok) {
+                throw new Error('failed to post comment')
+            }
+
+            return response
+        } catch (err) {
+            // give error message to user
+        } finally {
+            // 
+        }
+    }
+    function handleSubmit() {
+        // first handle request
+        // get feedback from backend and report to user
+        // refresh page
+    }
     return (
         <div id="make-comment-container">
-            <form>
-                <input id="comment-text-field" type="text" placeholder="Ăn khi ngồi luôn" />
+            <form action={handleSubmit}>
+                <input
+                id="comment-text-field" 
+                type="text" 
+                placeholder="Ăn khi ngồi luôn"
+                onChange={(e) => setText(e.target.value)} />
                 <br />
-                <input id="comment-range-stars" type="range" min={1} max={5} />
-                <br />
+                <input 
+                id="comment-range-stars" 
+                type="range" 
+                min={1} max={5} 
+                onChange={(e) => setStars(Number(e.target.value))}/>
                 <input id="comment-submit" type="submit" value="Send" />
             </form>
         </div>
